ac9cd06c8a8dd32b1492293a22c47487958f3644
brenosekiro <b88660501@gmail.com>
2020-10-22 18:11:47 -0300

    navigation

---
+++ src/pages/faq.tsx
@@ -0,0 +1,52 @@
+import { ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, Typography } from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { GetStaticProps } from "next"
+import { FaqModel } from "../../api/Faq"
+import { openDB } from "../openDB"
+
+interface faqProps {
+    faq: FaqModel[]
+}
+
+export default function Faq({ faq }: faqProps) {
+    return (
+        <div>
+            {faq.map(f => {
+                return (
+                    <div>
+                        {faq.map((f) => (
+                            <ExpansionPanel key={f.id}>
+                            <ExpansionPanelSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography>
+                                {f.question}
+                                </Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <Typography>
+                                {f.answer}
+                                </Typography>
+                            </ExpansionPanelDetails>
+                            </ExpansionPanel>
+                        ))}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+    const db = await openDB()
+    const faq = await db.all('SELECT * FROM FAQ ORDER BY createDate DESC')
+
+    return {
+        props: {
+            faq
+        }
+    }
+}
